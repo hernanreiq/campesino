fd42a60a04d5747f0e8e53d50d49962ac8329bf2
Hernan.Reiq <hernandemure1202@gmail.com>
2021-01-21 17:58:39 -0400

    Primera web hecha con bootstrap

--- js/script.js
+++ js/script.js
@@ -1,1 +1,71 @@
-console.log('hola')
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.header-content-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px',
+    rotate: {
+        x: 100,
+        z: 100
+    }
+
+});
+sr.reveal('.header-content-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.header-btn', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 1000
+});
+sr.reveal('#testimonio', {
+    duration: 2000,
+    origin: 'left',
+    distance: '500px'
+});
+sr.reveal('.info-img-right', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px',
+    rotate: {
+        x: -100,
+        z: -100
+    }
+
+});
+sr.reveal('.info-text-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.info-img-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px',
+    rotate: {
+        x: 100,
+        z: 100
+    }
+
+});
+sr.reveal('.info-text-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.card', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+});
+sr.reveal('#footer', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100px',
+});
